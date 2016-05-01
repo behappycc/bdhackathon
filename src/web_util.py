69bbf3587878b97e9ee344942d1d1d5b1119c2b0
b00902045 <b00902045@ntu.edu.tw>
2016-05-01 12:03:27 +0800

    update

--- src/web_util.py
+++ src/web_util.py
@@ -1,6 +1,8 @@
 import os
 import json
-
+import sys
+#reload(sys)
+#sys.setdefaultencoding(sys.getfilesystemencoding())
 def load_json(path):
     jsonFile = open(path,encoding='utf8')
     data = json.load(jsonFile)
@@ -10,4 +12,4 @@ def load_json(path):
 
 def write_json(data,path):
     with open(path, "w") as outfile:
-        json.dump(data, outfile, indent=4, ensure_ascii=False, sort_keys=True)
+        json.dump(data, outfile, indent=4, ensure_ascii=False, sort_keys=True).encode('utf8')
